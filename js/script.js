ce4e0870b664d2041d17cf9c94f5b1a4d2fd3f52
Frank Floyd Dbl <franzdblasi@gmail.com>
2017-12-07 18:07:13 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,74 +1,113 @@
-/*
- 
-4 compiti: 1) Aggiungere prodotti da una lista (no duplicati) col pulsante aggiungi;
-			  2) Eliminare prodotti da una lista (se esistono) col pulsante elimina;
-			  3) Visualizzare la lista con un apposito pulsante;
-			  4) Nascondere la lista con un apposito pulsante;
-			  
-*/
+// Seleziono gli elementi che utilizzo nel DOM:
+var input = document.querySelector(".inserisciProdotto");  // Seleziono l'input;
+var btnAggiungi = document.querySelector(".aggiungi");     // Seleziono pulsante "Aggiungi";
+var btnElimina = document.querySelector(".elimina");	   // Seleziono pulsante "Elimina";
+var btnSwitch = document.querySelector(".visLista");	   // Seleziono pulsante "VisLista";
+var registro = document.querySelector(".registro");        // Seleziono il div che contiene la lista;
+var txtArea = document.querySelector("textarea");          // Seleziono la textarea;
 
-// Selezione degli elementi del DOM su cui devo lavorare;
 
-var inputProdotti = document.querySelector('.insProdotto'); // Input
-var btnAggiungi = document.querySelector('.aggiungi'); // Pulsante Aggiungi
-var txtArea = document.querySelector('textarea'); // Textarea
-var visualizza = document.querySelector('.visualizzaLista'); // Pulsante Visualizza Shopping List
-// Mi creo una lista vuota (aka Array);
+// Inizializzo una lista (array) vuota;
 var listaProdotti = [];
 
-txtArea.style.display = 'none';
+// Nascondo di default il div contentente la lista
+registro.style.display = "none";
 
-// 1 - Aggiungere prodotti da una lista
-// ----------------------------------------------------------------------------------------
 
-// Al click del pulsante 'Aggiungi' (evento);
-btnAggiungi.addEventListener('click', function() {  
+// Quando clicco il pulsante "Aggiungi"
+btnAggiungi.addEventListener( "click", function() {
 
-		// Recupero la stringa che l'utente ha scritto nell'input e la salvo in una variabile;
-		var prodottoAggiunto = inputProdotti.value.toLowerCase();
-			// Se il campo non è vuoto allora
-			if ( prodottoAggiunto !== "" ) {
-				 // Inserisco la mia variabile all'interno dell'array (la mia lista);
-				 listaProdotti.push(prodottoAggiunto);
-				 
-				 var listaTxt = listaProdotti.join(", ");
-				 txtArea.value = listaTxt;
-			};
-		
-		// Svuoto il campo di inserimento prodotti per renderlo pronto per un nuovo inserimento
-		inputProdotti.value = "";  
 	
-});
-
-// Impostiamo una variabile che farà da interruttore per aprire o chiudere la nostra lista (falsa di default)
-var toggle = false;
-
-visualizza.addEventListener('click', function() {  
+	// Memorizzo il valore del campo input in una variabile;
+	var valoreInput = input.value;
 	
-	// Se l'interruttore è false (default) allora la lista è non visibile 
-	if ( toggle === false ) {
-		
-		// quindi rendo visibile la lista
-		txtArea.style.display = 'block';
-		// e imposta il mio interruttore a vero
-		toggle = true;
-		
-		// altrimenti, se è vero
-	} else {
-		// nascondo la lista
-		txtArea.style.display = 'none';
-		// imposto l'interruttore a falso
-		toggle = false;
+	// SE il campo input NON è vuoto allora
+	if ( valoreInput !== "" ) { 
+
+		// aggiungo questa variabile all'interno di una lista (array);
+		listaProdotti.push(valoreInput);
+		// converto l'array in una stringa memorizzandola in una variabile; 
+		var listaInStringa = listaProdotti.join(", ");
+		// assegno questa variabile come valore "value" della mia textarea
+		txtArea.value = listaInStringa;
+		// Svuoto il campo input
+		input.value = "";
+		// Metto in focus il campo input
+		input.focus();
 	}
-		
+	
+	// ALTRIMENTI
+	else {
+		// dammi un alert che mi dica che il campo è rimasto vuoto
+		alert("Attenzione, hai lasciato il campo vuoto");
+		// Metto in focus il campo input
+		input.focus();
+	}
+
 });
 
 
+// Quando clicco il pulsante "Elimina"
+btnElimina.addEventListener("click", function(){
+
+	// Memorizzo il valore del campo input in una variabile;
+	var valoreInput = input.value;
+	// Converto la mia stringa nella textarea in un array
+	var valoreTxtArea = txtArea.value;
+	listaProdotti = valoreTxtArea.split(", ");
+	// console.log(stringaInLista);
+	// memorizzo l'indice del valore che l'utente ha cercato nell'input
+	var index = listaProdotti.indexOf(valoreInput);
+
+	// SE all'interno del mio array c'è la voce (ovvero l'indice è diverso da -1)
+	if ( index !== -1 ) {
+		// allora faccio uno splice() sull'array togliendo quella voce
+		var spliced = listaProdotti.splice(index, 1);
+		// converto nuovamente l'array in stringa con join()
+		var listaInStringa = listaProdotti.join(", ");
+		// assegno il valore del nuovo array convertito al valore della textarea
+		txtArea.value = listaInStringa;
+		// svuoto il campo di input resettandolo ad una stringa vuota
+		input.value = "";
+		// do il focus sul campo di input
+		input.focus();
+		
+		// ALTRIMENTI
+	} else {
+		// Visualizzo un messagio di alert che indica che la voce che voglio eliminare non esiste
+		alert("Attenzione! La voce che vuoi eliminare non esiste");
+		// svuoto il campo di input
+		input.value = "";
+		// e reimposto il focus sull'input
+		input.focus();
+	}
+
+});
 
 
+// Inizializzo una variabile che userò come interruttore su FALSE
+var interruttore = false;
 
+// Quando clicco il pulsante "Visualizza Shopping List"
+btnSwitch.addEventListener("click", function(){
 
+	// SE la lista è nascosta (quindi interruttore === FALSE)
+	if ( interruttore === false) {
+		// visualizzo la lista
+		registro.style.display = "block"; 
+		// cambio il testo del pulsante da "Visualizza" a "Nascondi"
+		btnSwitch.innerText = "Nascondi shopping list";
+		// setto l'interruttore a TRUE
+		interruttore = true;
+	} 
+		// ALTRIMENTI
+	else {
+		// nascondo la lista
+		registro.style.display = "none";
+		// cambio il testo del pulsante da "Nascondi" a "Visualizza"
+		btnSwitch.innerText = "Visualizza shopping list";
+		// setto l'interruttore a FALSE
+		interruttore = false;
+	}
 
-
-
-
+});
